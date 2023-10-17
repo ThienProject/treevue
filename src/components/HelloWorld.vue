75ac4af8df434f9b74fae6ae58fbb7059c6fb53b
vanthienpham <vanthien.dev@gmail.com>
2023-10-17 17:41:41 +0700

    a

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,26 +9,7 @@ const treeData = ([
         key: '0-0-0',
         children: [
           {title: '0-0-0-0', key: '0-0-0-0'},
-          {
-            title: '0-0-0-1', key: '0-0-0-1',
-          },
-          {title: '0-0-0-2', key: '0-0-0-2'},
-        ],
-      },
-      {
-        title: 'parent 1-1',
-        key: '0-0-1',
-        children: [{title: 'leaf', key: '0-0-1-0'}],
-      },
-      {
-        title: 'parent 1-2',
-        key: '0-0-2',
-        children: [
-          {title: 'leaf 1', key: '0-0-2-0'},
-          {
-            title: 'leaf 2',
-            key: '0-0-2-1',
-          },
+          
         ],
       },
     ],
